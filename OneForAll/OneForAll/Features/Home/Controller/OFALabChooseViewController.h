df0efa49a9534662c43f1ca3928f1741fdac1187
Kira <735042473@qq.com>
2018-09-22 11:32:24 +0800

    【Feature】二级页面创建

---
+++ OneForAll/OneForAll/Features/Home/Controller/OFALabChooseViewController.h
@@ -0,0 +1,17 @@
+//
+//  OFALabChooseViewController.h
+//  OneForAll
+//
+//  Created by Kira on 2018/9/22.
+//  Copyright © 2018 Kira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OFALabChooseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
